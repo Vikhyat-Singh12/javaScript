0ae34802c73745c7f1124e82b7674eea9eed5a96
VIKHYAT SINGH <166906251+Vikhyat-Singh12@users.noreply.github.com>
2024-07-13 09:52:42 +0000

    container config done

---
+++ 01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"          // Its has different scope like Loacal & Globle
+var accountPassward = "123450"                  // Its has only one scope  i.e. Gloable
+accountCity = "Jaipur"                          // We can declare variable like this but this is a bad habbit.
+
+// accountId = 2                                // --> not allowed
+accountEmail = "hc@gmail.com";
+accountPassward = "12112212";
+accountCity = "Bengaluru";
+
+console.log(accountId);                      // --> This is used to get single output
+
+console.table([accountId, accountEmail, accountPassward , accountCity]);
